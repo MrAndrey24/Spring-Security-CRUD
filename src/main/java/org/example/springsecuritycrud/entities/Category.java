44db9efeb518ca26804947907b67368f9633c072
Andrey <andreyacostar@gmail.com>
2024-06-20 15:16:39 -0600

    feat: adds new roles and removes old default roles

---
+++ src/main/java/org/example/springsecuritycrud/entities/Category.java
@@ -0,0 +1,61 @@
+package org.example.springsecuritycrud.entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Table(name = "category")
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Product> product;
+
+    public Category() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Product> getProduct() {
+        return product;
+    }
+
+    public void setProduct(List<Product> product) {
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", product=" + product +
+                '}';
+    }
+}
